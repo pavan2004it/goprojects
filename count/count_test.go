f2df10103eeedebf7f6493ab64d7b91b5a1ca76a
Pavan Kumar <pavan_2004it@icloud.com>
2022-06-03 12:02:24 +0530

    Modified Lines function

--- count/count_test.go
+++ count/count_test.go
@@ -5,22 +5,6 @@ import (
 	"testing"
 )
 
-//func TestLines(t *testing.T) {
-//	t.Parallel()
-//	inputBuf := bytes.NewBufferString("1\n2\n3")
-//	c, err := count.NewCounter(
-//		count.WithInput(inputBuf),
-//	)
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//	want := 3
-//	got := c.Lines()
-//	if want != got {
-//		t.Errorf("want %d, got %d", want, got)
-//	}
-//}
-
 func TestWithInputFromArgs(t *testing.T) {
 	t.Parallel()
 	args := []string{"testdata/three_lines.txt"}
@@ -31,7 +15,7 @@ func TestWithInputFromArgs(t *testing.T) {
 		t.Fatal(err)
 	}
 	want := 7
-	got := c.MultiLines()
+	got := c.Lines()
 	if want != got {
 		t.Errorf("want %d, got %d", want, got)
 	}
@@ -46,7 +30,7 @@ func TestWithInputFromArgsEmpty(t *testing.T) {
 		t.Fatal(err)
 	}
 	want := 0
-	got := c.MultiLines()
+	got := c.Lines()
 	if want != got {
 		t.Errorf("want %d, got %d", want, got)
 	}
@@ -62,7 +46,7 @@ func TestWithInputFromMultiArgs(t *testing.T) {
 		t.Fatal(err)
 	}
 	want := 12
-	got := c.MultiLines()
+	got := c.Lines()
 	if want != got {
 		t.Errorf("want %d, got %d", want, got)
 	}
