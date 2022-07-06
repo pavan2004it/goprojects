cfffb4d3a9036d0ef5e7e18433b444336b3fadc3
Pavan Kumar <pavan_2004it@icloud.com>
2022-07-06 15:28:00 +0530

    removed old tests

--- azdocli/cmd/org/org_test.go
+++ azdocli/cmd/org/org_test.go
@@ -64,18 +64,4 @@ func TestUserCommand(t *testing.T) {
 	if string(got) != want {
 		t.Fatalf("expected %q got %q", want, string(got))
 	}
-
-	// Old Tests
-
-	//out, err := exec.Command("azdocli", "projects", "-o", "pavantikkani", "users").CombinedOutput()
-	//if err != nil {
-	//	t.Fatal(err)
-	//}
-	//want := "glaringfireball@gmail.com\npavan_2004it@hotmail.com\npavan2004it@gmail.com\n"
-	//got := bytes.NewBuffer(out)
-	//if want != got.String() {
-	//	t.Errorf("Wanted %q got %q", want, got)
-	//}
 }
-
-
