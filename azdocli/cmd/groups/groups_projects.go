5a9edf44f26a90de1acf69a80979b6fb8e29095b
Pavan Kumar <pavan_2004it@icloud.com>
2022-07-09 18:40:09 +0530

    removed prerun functions

--- azdocli/cmd/groups/groups_projects.go
+++ azdocli/cmd/groups/groups_projects.go
@@ -58,9 +58,6 @@ func NewProjectGroupsCommand() *cobra.Command {
 		Short: "List Security Groups in a Project",
 		Long:  "Lists Security Groups for a project in an organization",
 		RunE:  ListGroupsInProjects,
-		PreRun: func(cmd *cobra.Command, args []string) {
-			viper.BindPFlag("org", cmd.Flags().Lookup("org"))
-		},
 	}
 	projectConfig := pConfig{}
 	cmd.Flags().StringVarP(&projectConfig.project, "project", "p", "", "project name")
